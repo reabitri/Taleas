61a00ce2c6d00778e182f8d2f61b174c8e0418b0
reabitri <56025973+reabitri@users.noreply.github.com>
2022-07-25 13:22:39 +0200

    Update students.js

--- API/routes/students.js
+++ API/routes/students.js
@@ -1,107 +1,107 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const std = require("../models/student");
 
-const Student = require('../models/std');
-
-router.get('/', (req, res, next) => {
-    Student.find()
+router.get("/", (req, res, next) => {
+  std.find()
     .exec()
     .then(docs => {
-        console.log(docs);
-       
-            res.status(200).json(docs);
+      console.log(docs);
+        if (docs.length >= 0) {
+      res.status(200).json(docs);
+        } else {
+            res.status(404).json({
+                message: 'No entries found'
+             });
+         }
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
 router.post("/", (req, res, next) => {
-    const std = new Student({
-        _id: new mongoose.Types.ObjectId(),
-        Name: req.body.Name,
-        Department: req.body.Department,
-        Age: req.body.Age
-    });
-    std
+  const student = new std({
+    _id: new mongoose.Types.ObjectId(),
+    Name: req.body.Name,
+    Department: req.body.Department,
+    Age: req.body.Age
+  });
+  student
     .save()
     .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message:"Post request",
-            createdStd: result
-        });
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST requests to /products",
+        createdProduct: result
+      });
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Student.findById(id)
+router.get("/:studentId", (req, res, next) => {
+  const id = req.params.productId;
+  std.findById(id)
     .exec()
     .then(doc => {
-        console.log("From database", doc);
-        if(doc){
-            res.status(200).json(doc);
-        }else{
-            res.status(404).json({message: 'No valid entry for provided ID'});
-        }
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err});
-
-});
-});
-
-
-router.patch("/:productId", (req, res, next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-    for(const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-    Student.updateOne({_id: id}, {$set: updateOps})
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(200).json(result);
-    })
-    .catch(err=> {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-    
 });
 
-
-router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Student.remove({ _id: id })
+router.patch("/:studentId", (req, res, next) => {
+  const id = req.params.studentId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  std.update({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-        res.status(200).json(result);
+      console.log(result);
+      res.status(200).json(result);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
+});
+
+router.delete("/:studentId", (req, res, next) => {
+  const id = req.params.studentId;
+  std.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
+});
 
 module.exports = router;
